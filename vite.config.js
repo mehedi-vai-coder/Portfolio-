c8d40b49635542b8ae183021f953a2a939402968
MD MEHEDI HASAN <md.mehedihasan8n@gmail.com>
2025-06-28 21:24:22 +0600

    Blur Animation added

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,22 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  theme: {
+    extends: {
+      keyframes: {
+        blob: {
+          '0%': { transform: 'scale(1)' },
+          '33%': { transform: 'scale(1.2)' },
+          '66%': { transform: 'scale(0.8)' },
+          '100%': { transform: 'scale(1)' },
+        },
+      },
+      animation: {
+        blob: 'blob 10s infinite',
+      },
+      backgroundImage: {
+        'skills-gradient': 'linear-gradient(38.73deg, rgba(204, 0, 187, 0.15) 0%, rgba(201, 32, 184, 0) 50%), linear-gradient(141.27deg, rgba(0, 70, 209, 0) 50%, rgba(0, 70, 209, 0.15) 100%)',
+      },
+    },
+  },
 })
